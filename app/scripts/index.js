79c427ce7d702bb6ce69527cc832b1f4d06ea53d
Robert-Jan Keizer <robertjankeizer@gmail.com>
2015-10-26 14:54:24 +0100

    Added sammy

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,7 +1,27 @@
-'use strict';
-document.addEventListener('DOMContentLoaded', function() {
-  var h1 = document.getElementsByTagName('h1');
-  if (h1.length > 0) {
-    h1[0].innerText = h1[0].innerText + ' \'Allo';
-  }
-}, false);
+;(function($) {
+    var app = $.sammy(function() {
+        this.get('/', function(context) {
+            $.get('templates/school.hbs', function (templateData) {
+                var template = Handlebars.compile(templateData);
+                $('.page-content').html(template(JSON.parse("{}")));
+                setTimeout(function() {
+                    context.redirect('#', "login");
+                    // body...
+                }, 2000)
+            });
+        });
+        
+        this.get('/#/login', function() {
+            $.get('templates/login.hbs', function (templateData) {
+                var template = Handlebars.compile(templateData);
+                $('.page-content').html(template(JSON.parse("{}")));
+                $('.page-content input').get(0).MaterialTextfield.change('');
+            });
+        });
+    });
+
+// start the application
+  $(function() {
+    app.run()
+  });
+})(jQuery);
