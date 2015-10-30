fa3467a23ef5ad65ce8fc25e8631218cdbbc17de
Robert-Jan Keizer <robertjankeizer@gmail.com>
2015-10-30 13:00:33 +0100

    Updated some dat

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,4 +1,11 @@
-// ;(function($) {
-//   $(function() {
-//   });
-// })(jQuery);
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://mijn.magister.net/api/schools?filter=farel", true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 4) {
+    // JSON.parse does not evaluate the attacker's scripts.
+    var text = document.createElement("div");
+    text.innerHTML = JSON.stringify(JSON.parse(xhr.responseText));
+    document.body.appendChild(text);
+  }
+}
+xhr.send();
