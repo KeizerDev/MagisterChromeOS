eab4177b532f4e418c82d29cc1136bc55921a52f
Robert-Jan Keizer <robertjankeizer@gmail.com>
2015-10-30 13:54:17 +0100

    Updated wip

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,11 +1,21 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://mijn.magister.net/api/schools?filter=farel", true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4) {
-    // JSON.parse does not evaluate the attacker's scripts.
-    var text = document.createElement("div");
-    text.innerHTML = JSON.stringify(JSON.parse(xhr.responseText));
-    document.body.appendChild(text);
-  }
-}
-xhr.send();
+'use strict';
+$( document ).ready(function() {
+
+    console.log( 'ready!' );
+});
+
+// function searchSchool(qry) {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'https://mijn.magister.net/api/schools?filter=' + qry, true);
+//     xhr.onreadystatechange = function() {
+//       if (xhr.readyState === 4) {
+//         // JSON.parse does not evaluate the attacker's scripts.
+//         console.log(xhr.responseText);
+//         var resp = JSON.parse(xhr.responseText);
+//         var text = document.createElement('div');
+//         text.innerHTML = JSON.stringify(resp);
+//         document.body.appendChild(text);
+//       }
+//     };
+//     xhr.send();
+//   }
