60a55c2be00258a9557fa4104227d8264eb52030
Robert-Jan Keizer <robertjankeizer@gmail.com>
2015-11-02 23:40:03 +0100

    Made selection work

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,18 +2,24 @@
 $( document ).ready(function() {
 
     $(document).on('change', '#schoolsearch', function(e) {
-        $.get("https://mijn.magister.net/api/schools?filter=" + $('#schoolsearch').val(), function(res) {
+        $.get('https://mijn.magister.net/api/schools?filter=' + $('#schoolsearch').val(), function(res) {
             $('.schoolresult').html('');
             if (res.length == 0) {
-                $('<li class="list-group-item">Geen school gevonden</li>').appendTo('.schoolresult');
+                $('<label for="" disabled><input type="radio" value="accessible" name="quality" id=""><span>Geen school gevonden</span></label>').appendTo('.schoolresult');
             } else {   
                 $.each(res, function(index, val) {
                     var reg = new RegExp('https://([^<]*).magister.net', 'g');
-                    $('<li class="list-group-item selected" data-schoolid="'+ reg.exec(val.Url)[1] +'">'+ val.Name +'</li>').appendTo('.schoolresult');
+                    $('<label for="'+ val.Id +'"><input type="radio" value="accessible" name="quality" id="'+ val.Id +'"><span>'+ reg.exec(val.Url)[1] +'</span></label>').appendTo('.schoolresult');
                 });
             }
             console.log(res);
         });
+        e.preventDefault();
+    });
+
+    $(document).on('click', '.schoolresult li', function(e) {
+        console.log(e); 
+        console.log($(e.target).addClass('selected'));
         e.preventDefault();
     });
 
