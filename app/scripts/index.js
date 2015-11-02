5ded6f7e6a88ab92bc277c8dc9d85501af009185
Robert-Jan Keizer <robertjankeizer@gmail.com>
2015-11-02 10:51:21 +0100

    Updated jquery

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,20 +2,20 @@
 $( document ).ready(function() {
 
     console.log( 'ready!' );
-});
 
-// function searchSchool(qry) {
-//     var xhr = new XMLHttpRequest();
-//     xhr.open('GET', 'https://mijn.magister.net/api/schools?filter=' + qry, true);
-//     xhr.onreadystatechange = function() {
-//       if (xhr.readyState === 4) {
-//         // JSON.parse does not evaluate the attacker's scripts.
-//         console.log(xhr.responseText);
-//         var resp = JSON.parse(xhr.responseText);
-//         var text = document.createElement('div');
-//         text.innerHTML = JSON.stringify(resp);
-//         document.body.appendChild(text);
-//       }
-//     };
-//     xhr.send();
-//   }
+    function searchSchool(qry) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://mijn.magister.net/api/schools?filter=' + qry, true);
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4) {
+            // JSON.parse does not evaluate the attacker's scripts.
+            console.log(xhr.responseText);
+            var resp = JSON.parse(xhr.responseText);
+            var text = document.createElement('div');
+            text.innerHTML = JSON.stringify(resp);
+            document.body.appendChild(text);
+          }
+        };
+        xhr.send();
+    }
+});
