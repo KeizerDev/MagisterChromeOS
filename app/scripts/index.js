3875ec1694b461886c2969e10c8756f2e8745ea8
Robert-Jan Keizer <robertjankeizer@gmail.com>
2015-11-05 19:10:37 +0100

    Made handlebars works

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,38 +1,63 @@
-'use strict';
+// 'use strict';
 
 
-chrome.browserAction.onClicked.addListener(function() {
- var iframe = document.getElementById('theFrame');
- var message = {
-   command: 'render',
-   context: {thing: 'world'}
- };
- iframe.contentWindow.postMessage(message, '*');
+var counter=0;
+document.addEventListener('DOMContentLoaded', function() {
+
+  document.getElementById('reset').addEventListener('click', function(event) {
+    counter=0;
+    document.querySelector("#result").innerHTML="";
+  });
+
+  document.getElementById('sendMessage').addEventListener('click', function(event) {
+    counter++;
+    var message = {
+      command: 'render',
+      templateName: 'schoollogin',
+      context: {'counter': counter}
+    };
+
+    var message1 = {
+      command: 'render',
+      templateName: 'schoolkiezen',
+      context: {'counter': counter}
+    };
+    document.getElementById('theFrame').contentWindow.postMessage(message, '*');
+
+    setTimeout(function() {
+    document.getElementById('theFrame').contentWindow.postMessage(message1, '*');
+    }, 1000);
+  });
+
+  // on result from sandboxed frame:
+  window.addEventListener('message', function(event) {
+    document.querySelector("#result").innerHTML=event.data.result || "invalid result"
+  });
 });
 
 
-$( document ).ready(function() {
-    var tempSchoolkiezen = Handlebars.compile($("#schoolkiezen").html());
-    var tempSchoollogin = Handlebars.compile($("#schoollogin").html());
+// $( document ).ready(function() {
+//     var tempSchoolkiezen = Handlebars.compile($("#schoolkiezen").html());
+//     var tempSchoollogin = Handlebars.compile($("#schoollogin").html());
 
-    setTimeout(function() {
-        $('.login-container').html(tempSchoolkiezen());
-        $('#schoolsearch').on('change', function(e) {
-            $.get('https://mijn.magister.net/api/schools?filter=' + $('#schoolsearch').val(), function(res) {
-                $('.schoolresult').html(tempSchoolkiezen(res));
-                if (res.length == 0) {
-                    $('<label for="" disabled><input type="radio" value="accessible" name="quality" id=""><span>Geen school gevonden</span></label>').appendTo('.schoolresult');
-                } else {
-                    $.each(res, function(index, val) {
-                        var reg = new RegExp('https://([^<]*).magister.net', 'g');
-                        $('<label for="'+ val.Id +'"><input type="radio" value="accessible" name="quality" id="'+ val.Id +'"><span>'+ reg.exec(val.Url)[1] +'</span></label>').appendTo('.schoolresult');
-                    });
-                }
-                console.log(res);
-            });
-            e.preventDefault();
-        });
-    }, 1000);
+//     setTimeout(function() {
+//         $('.login-container').html(tempSchoolkiezen());
+//         $('#schoolsearch').on('change', function(e) {
+//             $.get('https://mijn.magister.net/api/schools?filter=' + $('#schoolsearch').val(), function(res) {
+//                 $('.schoolresult').html(tempSchoolkiezen(res));
+//                 if (res.length == 0) {
+//                     $('<label for="" disabled><input type="radio" value="accessible" name="quality" id=""><span>Geen school gevonden</span></label>').appendTo('.schoolresult');
+//                 } else {
+//                     $.each(res, function(index, val) {
+//                         var reg = new RegExp('https://([^<]*).magister.net', 'g');
+//                         $('<label for="'+ val.Id +'"><input type="radio" value="accessible" name="quality" id="'+ val.Id +'"><span>'+ reg.exec(val.Url)[1] +'</span></label>').appendTo('.schoolresult');
+//                     });
+//                 }
+//                 console.log(res);
+//             });
+//             e.preventDefault();
+//         });
+//     }, 1000);
 
 //     $(document).on('click', '.schoolresult li', function(e) {
 //         console.log(e);
@@ -76,7 +101,7 @@ $( document ).ready(function() {
 //             });
 //         }
 //     });
-});
+// });
 
 // $.ajax({
 //     url: 'https://ams.magister.net/api/personen/5431/afspraken?status=1&tot=2015-11-04&van=2015-11-03',
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,13 +2,13 @@
 
 // Listens for the app launching then creates the window
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('basic.html', {
+  chrome.app.window.create('index.html', {
     id: 'main',
-    bounds: {width: 700, height: 525},
+    bounds: {width: 700, height: 500},
     minWidth: 350,
-    minHeight: 525,
+    minHeight: 500,
     maxWidth: 700,
-    maxHeight: 525,
+    maxHeight: 500,
     frame: {
         type: 'chrome',
         color: '#2980b9',
