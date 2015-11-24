c57b05c2b35eeedc1df7e41005e0f5d55d4f661c
abcroes <AntoineHD@icloud.com>
2015-11-24 09:55:03 +0100

    add some things

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
+
 var userAgent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16";
+var school;
 
 $( document ).ready(function() {
     // chrome.storage.local.set({'firstrun': true}, function() {});
