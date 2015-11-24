b2102413ca23353bfc851c6b6d2faf6d48a28f8a
Robert-Jan Keizer <robertjankeizer@gmail.com>
2015-11-24 09:37:42 +0100

    Fixed iframe stuff

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var school;
+var userAgent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16";
 
 $( document ).ready(function() {
     // chrome.storage.local.set({'firstrun': true}, function() {});
@@ -32,8 +32,20 @@ function viewFirstrun() {
 
 function viewMagisterframe() {
     var tempMgstrwebview = $('#magisterframe').html();
-    chrome.storage.local.get('school', setupWebview);
     $('#app').html(Mustache.render(tempMgstrwebview, {}));
+    chrome.storage.local.get('school', setupWebview);
+
+    // var loadstart = function() {
+    //     // indicator.innerText = "loading...";
+    // }
+
+    // var loadstop = function() {
+    //     // indicator.innerText = "";
+    // }
+
+    // webview.addEventListener("loadstart", loadstart);
+    // webview.addEventListener("loadstop", loadstop);
+
 }
 
 function saveData(obj) {
@@ -46,7 +58,12 @@ function saveData(obj) {
 }
 
 function setupWebview(e) {
-    console.log(e.school)
+    var webview = document.getElementById("magisterview");
+
+    webview.src = "https://" + e.school + ".magister.net/";
+    webview.setUserAgentOverride(userAgent);
+    console.log(webview);
+    // var indicator = document.querySelector(".indicator");
 }
 
 
