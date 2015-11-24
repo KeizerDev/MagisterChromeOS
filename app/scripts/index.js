b5b86e169bf22ab5067c951274bb4f0de8227031
Robert-Jan Keizer <robertjankeizer@gmail.com>
2015-11-24 09:08:27 +0100

    Finnished some part of the js

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,85 +3,77 @@
 var school;
 
 $( document ).ready(function() {
-    chrome.storage.local.set({'firstrun': false}, function() {});
+    // chrome.storage.local.set({'firstrun': true}, function() {});
+    chrome.storage.local.get('firstrun', isFirstrun);
+});
 
-    chrome.storage.local.get('firstrun', function(e) {
-        console.log(e)
-        if (e.firstrun == true) {
-            console.log(true);
-        } else {
-            console.log(false);
-        };
-    });
-
-
-// function containerSetup(isFirstrun) {
-//     $('#app').html();
-//     if (isFirstrun) {
-//         var tempSchoollogin = $('#schoolcontainer').html();
-//         $('#app').html(Mustache.render(tempSchoollogin, {}));
-//     } else {
-//         var tempSchooldash = $('#schooldashboard').html();
-//         $('#app').html(Mustache.render(tempSchooldash, {}));
-//     }
-// }
-
-
-// function firstrun() {
+function viewFirstrun() {
     var tempMgstrContainer = $('#schoolcontainer').html();
-    var tempMgstrlogin = $('#schoollogin').html();
     var tempMgstrkiezen = $('#schoolkiezen').html();
 
-    var rendered = Mustache.render(tempMgstrContainer, {});
-    $('#app').html(rendered);
-
+    // Basic view for not logged in users
+    $('#app').html(Mustache.render(tempMgstrContainer, {}));
     $('.login-container').html(Mustache.render(tempMgstrkiezen, {}));
-    $('#schoolsearch').on('input', function(e) {
-        $.get('https://mijn.magister.net/api/schools?filter=' + $('#schoolsearch').val(), function(res) {
-            $('.schoolresult').html('');
-            if (res.length == 0) {
-                $('<label for="" disabled><input type="radio" name="school" id=""><span>Geen school gevonden</span></label>').appendTo('.schoolresult');
-            } else {
-                $.each(res, function(index, val) {
-                    var reg = new RegExp('https://([^<]*).magister.net', 'g');
-                    $('<label for="'+ val.Id +'"><input type="radio" name="school" data-schoolname="'+ reg.exec(val.Url)[1] +'" id="'+ val.Id +'"><span>'+ val.Name +'</span></label>').appendTo('.schoolresult');
-                });
-            }
-            console.log(res);
-        });
 
-        e.preventDefault();
-    });
-
-
+    // Extra handlers for above view
+    $('#schoolsearch').on('input', searchSchool);
     $(document).on('click', '.btn-submit-school', function(event) {
         if ($('.schoolresult input[name="school"]:checked', '.mgstr-school').data('schoolname') != undefined) {
             school = $('.schoolresult input[name="school"]:checked', '.mgstr-school').data('schoolname');
-            // Start iframe
-            console.log('lol');
+
+            saveData({'firstrun': false});
+            saveData({'school': school});
+            // Do things to store them and start iframe
+            viewMagisterframe();
         };
         event.preventDefault();
     });
+}
 
-});
-
+function viewMagisterframe() {
+    var tempMgstrwebview = $('#magisterframe').html();
+    chrome.storage.local.get('school', setupWebview);
+    $('#app').html(Mustache.render(tempMgstrwebview, {}));
+}
 
 function saveData(obj) {
-    // Pass some sort of object:
+    // Pass object:
     // {'value': theValue}
 
-    // Save it using the Chrome extension storage API.
+    // Set obj
     chrome.storage.local.set(obj, function() {
     });
 }
 
-function getChanges(str) {
-    // get obj:
-    // {'value': theValue}
+function setupWebview(e) {
+    console.log(e.school)
+}
 
-    // Save it using the Chrome extension storage API.
-    chrome.storage.local.get(str, function(e) {
-        console.log(e);
-        // return e;
+
+function isFirstrun(e) {
+    console.log(e);
+    if (e.firstrun == false) {
+        console.log("firstrun:" + false);
+        viewMagisterframe();
+    } else {
+        console.log("firstrun:" + true);
+        viewFirstrun();
+    };
+}
+
+function searchSchool(e) {
+    $.get('https://mijn.magister.net/api/schools?filter=' + $('#schoolsearch').val(), function(res) {
+        $('.schoolresult').html('');
+        if (res.length == 0) {
+            $('<label for="" disabled><input type="radio" name="school" id=""><span>Geen school gevonden</span></label>').appendTo('.schoolresult');
+        } else {
+            $.each(res, function(index, val) {
+                var reg = new RegExp('https://([^<]*).magister.net', 'g');
+                $('<label for="'+ val.Id +'"><input type="radio" name="school" data-schoolname="'+ reg.exec(val.Url)[1] +'" id="'+ val.Id +'"><span>'+ val.Name +'</span></label>').appendTo('.schoolresult');
+            });
+        }
+        console.log(res);
     });
+
+    e.preventDefault();
 }
